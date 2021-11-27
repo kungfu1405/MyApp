481c086d34040c4cf7f8bd34ac79a1d5cc7acfe4
huy nv <huytb2011@gmail.com>
2021-11-27 11:10:49 +0700

    first Worksimple

---
+++ WorkSimple/WebMvc/Data/Migrations/20211126060200_Chang identity User.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebMvc.Data.Migrations
+{
+    public partial class ChangidentityUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ WorkSimple/WebMvc/Data/WebUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebMvc.Data
+{
+    public class WebUser:IdentityUser
+    {
+        public string CustomTag { get; set; }
+        public string DOB { get; set; }
+    }
+}
