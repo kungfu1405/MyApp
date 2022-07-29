27bb7960591153df8c51ab585d8e8094fc2a0da8
huy nv <huytb2011@gmail.com>
2022-07-29 15:10:22 +0700

    up daily

---
+++ ExampleNetCore/ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    public interface text
+    {
+        void print();
+    }
+    public class format:text
+    {
+        public void print()
+        { Console.WriteLine(" here is text format"); }
+    }
+    // constructor injection
+    public class constructorinjection
+    {
+        private text _text;
+        public constructorinjection(text t1)
+        {
+            this._text = t1;
+        }
+        public void print()
+        {
+            _text.print();
+        }
+    }
+        class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+  
+  
+   
+}
---
+++ ExampleNetCore/WebApplication1/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using WebApplication1.Data;
+using WebApplication1.Models;
+
+namespace WebApplication1.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly WebApplication1.Data.WebApplicationContext _context;
+
+        public IndexModel(WebApplication1.Data.WebApplicationContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; }
+
+        //public async Task OnGetAsync()
+        //{
+        //    Movie = await _context.Movie.ToListAsync();
+        //}
+        public IList<Movie> OnGetAsync()
+        {
+            return Movie = _context.Movie.ToList();
+        }
+    }
+}
